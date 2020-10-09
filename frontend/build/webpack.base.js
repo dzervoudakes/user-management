0c3ff1d6f571f6d2f1f650b0a03f1c00f7c771ae
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2020-10-08 19:27:35 -0600

    bundlers => bundling

--- frontend/build/webpack.base.js
+++ frontend/build/webpack.base.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Base Webpack configuration, shared between development and production builds.
- * - Default config: https://github.com/dzervoudakes/dztools/blob/master/packages/bundlers/webpack-config-react-typescript/config/webpack.base.js
- * - Notes on customizing/extending: https://github.com/dzervoudakes/dztools/tree/master/packages/bundlers/webpack-config-react-typescript#readme
+ * - Default config: https://github.com/dzervoudakes/dztools/blob/master/packages/bundling/webpack-config-react-typescript/config/webpack.base.js
+ * - Notes on customizing/extending: https://github.com/dzervoudakes/dztools/tree/master/packages/bundling/webpack-config-react-typescript#readme
  */
 const { webpackBaseConfig } = require('@dztools/webpack-config-react-typescript');
 
--- frontend/build/webpack.dev.js
+++ frontend/build/webpack.dev.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Webpack development configuration.
- * - Default config: https://github.com/dzervoudakes/dztools/blob/master/packages/bundlers/webpack-config-react-typescript/config/webpack.dev.js
- * - Notes on customizing/extending: https://github.com/dzervoudakes/dztools/tree/master/packages/bundlers/webpack-config-react-typescript#readme
+ * - Default config: https://github.com/dzervoudakes/dztools/blob/master/packages/bundling/webpack-config-react-typescript/config/webpack.dev.js
+ * - Notes on customizing/extending: https://github.com/dzervoudakes/dztools/tree/master/packages/bundling/webpack-config-react-typescript#readme
  */
 const { webpackDevConfig } = require('@dztools/webpack-config-react-typescript');
 
