1462777241de20dadf714cb3ca35141f126bbc1d
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2021-01-31 15:31:09 -0700

    Add config files for server

---
+++ server/.eslintrc.js
@@ -0,0 +1,4 @@
+// Default config: https://github.com/dzervoudakes/dztools/blob/main/packages/formatting/eslint-config-typescript/index.js
+module.exports = {
+  extends: ['@dztools/eslint-config-typescript']
+};
---
+++ server/.prettierrc.js
@@ -0,0 +1,4 @@
+// Default config: https://github.com/dzervoudakes/dztools/blob/main/packages/formatting/prettier-config/index.js
+const config = require('@dztools/prettier-config');
+
+module.exports = config;
