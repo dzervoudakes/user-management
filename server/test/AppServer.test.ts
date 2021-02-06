72113002a2f7da9a698ec1f203812d7e83d3d826
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2021-02-06 09:51:27 -0700

    Add UserController test to AppServer test suite

--- server/test/AppServer.test.ts
+++ server/test/AppServer.test.ts
@@ -2,9 +2,18 @@ import supertest, { SuperTest, Test } from 'supertest';
 import StatusCodes from 'http-status-codes';
 import { Application } from 'express';
 import AppServer from '@src/AppServer';
+import { mockUserOne, mockUserTwo } from './utils';
+
+jest.mock('@overnightjs/core', () => ({
+  ...(jest.requireActual('@overnightjs/core') as jest.Mock),
+  Middleware: () => jest.fn()
+}));
 
 jest.mock('@overnightjs/jwt', () => ({
-  JwtManager: { jwt: jest.fn().mockImplementation(() => 'i am a token') }
+  JwtManager: {
+    jwt: jest.fn().mockImplementation(() => 'i am a token'),
+    middleware: jest.fn()
+  }
 }));
 
 jest.mock('mongoose', () => ({
@@ -12,15 +21,28 @@ jest.mock('mongoose', () => ({
   connect: jest.fn()
 }));
 
-jest.mock('@src/controllers/UserController');
-
 const mockGetAdmin = jest.fn();
+const mockGetUsers = jest.fn();
 
 jest.mock('@src/daos/AdminDao', () => {
   return {
     AdminDao: jest.fn().mockImplementation(() => {
       return {
         getAdmin: mockGetAdmin
+      };
+    })
+  };
+});
+
+jest.mock('@src/daos/UserDao', () => {
+  return {
+    UserDao: jest.fn().mockImplementation(() => {
+      return {
+        getUsers: mockGetUsers,
+        getUser: jest.fn(),
+        createUser: jest.fn(),
+        updateUser: jest.fn(),
+        deleteUser: jest.fn()
       };
     })
   };
@@ -40,6 +62,10 @@ describe('AppServer', () => {
     agent = supertest.agent(server.getExpressInstance());
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('calls the auth controller', async () => {
     mockGetAdmin.mockImplementationOnce(() => ({
       username: 'username',
@@ -49,5 +75,13 @@ describe('AppServer', () => {
 
     expect(result.status).toBe(StatusCodes.OK);
     expect(result.body.token).toEqual('i am a token');
+  });
+
+  it('calls the user controller', async () => {
+    mockGetUsers.mockImplementationOnce(() => [mockUserOne, mockUserTwo]);
+    const result = await agent.get('/api/users');
+
+    expect(result.status).toBe(StatusCodes.OK);
+    expect(result.body.users).toEqual([mockUserOne, mockUserTwo]);
   });
 });
